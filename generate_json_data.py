6c2e694dc887234f98ac3093d8f995fc314e5aa7
Jordi Smit <jordismit20000@gmail.com>
2021-11-19 09:47:22 +0100

    pytorch grouping

--- generate_json_data.py
+++ generate_json_data.py
@@ -3,7 +3,11 @@ from pathlib import Path
 
 def main():
     root = Path(__file__).parent
-    dirs = [path.parent.name for path in root.rglob("cookiecutter.json")]
+    dirs = []
+    for path in root.rglob("cookiecutter.json"):
+        dirs.append(str(path.parent).replace(str(root) + "/", ""))
+
+    dirs = sorted(dirs)
 
     with open(root / "info.json", "w") as f:
         json.dump({"dirs": dirs}, f, indent=2)
