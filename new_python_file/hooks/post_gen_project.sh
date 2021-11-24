e19b84be2a3e5fe16bafcdf191b86bbdbdb81b51
Jordi Smit <jordismit20000@gmail.com>
2021-11-24 12:32:02 +0100

    removed old python template

--- new_python_file/hooks/post_gen_project.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-file_type='{{cookiecutter.type}}'
-
-if [ "$file_type" == "runable" ]; then
-	find . -maxdepth 1 -type f -name "*.py" | xargs chmod u+x 
-fi
-cp -rf . ..
-rm -rf -- "$(pwd -P)"
